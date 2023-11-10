8d12c3d5b986e7d0a8cb72a6780f178cd1d8e84f
김태훈 <je64768035@gmail.com>
2023-11-10 14:33:59 +0900

    SUIT 폰트 추가

--- src/assets/styles/GlobalStyles.jsx
+++ src/assets/styles/GlobalStyles.jsx
@@ -2,9 +2,9 @@ import { createGlobalStyle } from 'styled-components'
 /*** font ***/
 // 참고 url : https://sunn.us/suit/
 export const GlobalStyles = createGlobalStyle`
+body,p,h1,h2,h3,h4,h5,h6,ul,ol,li,dl,dt,dd,div,span,table,th,td,form,fieldset,legend,input,textarea,button,select,svg,i {margin:0;padding:0;box-sizing:border-box; font-family:'SUIT', sans-serif;}
   body {
     font-size: 16px;
-    font-family:'SUIT', sans-serif;
   }
   img {
     vertical-align: top;
