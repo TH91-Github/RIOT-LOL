287cffd0294621543ece4e412ef1908f42c5d3a5
김태훈 <je64768035@gmail.com>
2023-11-30 15:42:19 +0900

    테스트 url 임시 저장

--- src/component/pages/MainSearch.jsx
+++ src/component/pages/MainSearch.jsx
@@ -1,33 +1,33 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import Search from "component/common/Search";
 import styled from "styled-components";
 import { colors, transitions } from "assets/styles/Variable";
 import { LOL_API_SUMMONER } from "constants/ApiConfig";
 import { loadAxios } from "utils/api";
 
-  const TESTNAME ="게보린";
-
   const MainSearch = () => {
   const [mouseOn, setMouseOn] = useState(false);
+  const [error, setError] = useState(false);
 
-  const loadData = useCallback(async () => {
-    const res = await loadAxios(LOL_API_SUMMONER(TESTNAME));
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const loadData = useCallback(async (searchName) => {
+    // const res = await loadAxios(LOL_API_SUMMONER(searchName));
+    const res = await loadAxios(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/1lYtsnwkGp_0E2o1M6hZIj38IAxiXwFIdV_H0z1aLRDQ8w?api_key=${API_KEY}`);
     console.log(res.data)
   }, []);
-
-  useEffect(() => {
-    loadData();
-  }, [loadData]);
-
-  const searchResult = (e) => {
+  
+  const searchResult = (e) => { // 검색을 받아온다 -> 유저 정보
     console.log(e)
+    loadData(e)
   }
-
+  
   return (
     <MainSearchWrap>
       <MainSearchInner>
         {/* 눈 모양 */}
-        <EyeWrap className={mouseOn && "on"}>
+
+        {/* <EyeWrap className={mouseOn && "on"}>
           <EyeBox className="eye__box">
             <EyeL />
             <EyeR />
@@ -36,7 +36,7 @@ import { loadAxios } from "utils/api";
             <EyeL />
             <EyeR />
           </EyeBox>
-        </EyeWrap>
+        </EyeWrap> */}
         {/* 유저 검색 */}
         <UserSerach>
           <Search placeholder="소환사명" mouseAction={setMouseOn} propsEvent={searchResult}/>
@@ -71,7 +71,7 @@ const EyeWrap = styled.div`
   gap:80px;
   position:relative;
   padding: 32px 0;
-  margin:50px auto;
+  margin:0 auto 50px;
   &.on {
     .eye__box {
       & > * {
