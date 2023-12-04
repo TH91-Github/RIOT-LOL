e56899f93d9362e22d47b25592dbcd82d0127587
김태훈 <je64768035@gmail.com>
2023-12-04 14:55:22 +0900

    Summoners 구조 세팅 주석

--- src/component/pages/Summoners.jsx
+++ src/component/pages/Summoners.jsx
@@ -9,7 +9,34 @@ function Summoners () {
   const user = useSelector((state) => state.userData ); // 소환사 정보
   console.log(user)
 
-  
+  // const loadData = async (searchName) => {
+  //   setLoading(true);
+  //   const upLoadData = {
+  //     info : null,
+  //     league : null,
+  //     match : null,
+  //     gameInfo : null
+  //   }
+  //   try {
+  //     // 1차 유저 이름 검색 - 기본 정보
+  //     const userInfo = await loadAxios(LOL_API_SUMMONER(searchName));
+  //     upLoadData.info = userInfo.data;
+  //     // 2차 유저 리그 정보
+  //     const userLeague = await loadAxios(LOL_API_LEAGUE(userInfo.data.id));
+  //     upLoadData.league = userLeague.data;
+  //     // 3차 매치 정보
+  //     const userMatchId = await loadAxios(LOL_API_MATYCH_ID(userInfo.data.puuid))
+  //     upLoadData.match = userMatchId.data;
+  //     dispatch(sSetUserData(upLoadData))
+  //     setLoading(false);
+  //     setError(false);
+  //     console.log("소환사 검색 성공");
+  //     navigate(`/summoners/kr/${upLoadData.info.name}-KR1`); // 페이지 이동
+  //   }catch(error){
+  //     console.log(error);
+  //     setError(true);
+  //   }
+  // };
 
   useEffect(()=> { // 유저 정보가 없다면 불러오기
 
