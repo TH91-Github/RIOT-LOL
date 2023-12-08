881227301b26f0b217947bd4c10fd2c29245d047
김태훈 <je64768035@gmail.com>
2023-12-08 09:16:09 +0900

    [LOL] 스크롤 테스트 원복

--- src/component/RiotApp.jsx
+++ src/component/RiotApp.jsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet, useLocation } from "react-router-dom";
 import { sSetMobileChk } from "store/store";
-import styled from "styled-components";
 // import { loadAxios } from "utils/fetchAxios";
 import { isMobile } from "utils/common.js"
 const RiotMain = () => {
@@ -26,56 +25,9 @@ const RiotMain = () => {
   return (
     <div className="wrap">
       <div className="container">
-        TEST 중입니다.555555
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p><p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <p>estsetsetsetsetsetsetsetse</p>
-        <TestBox>
-          <div className="box">
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-            <p>estsetsetsetsetsetsetsetse</p>
-
-          </div>
-        </TestBox>  
-        {/* <Outlet context={location} /> */}
+        <Outlet context={location} />
       </div>
     </div>
   )
 }
 export default RiotMain;
-
-
-const TestBox = styled.div`
-height:100vh;
-  .box {
-    overflow: auto;
-    width:100%;
-    max-width:152px;
-    height:100px;
-    margin:0 auto;
-    border:1px solid green;
-    box-sizing:border-box;
-    background:black;
-  }
-  
-  color:#fff;
-`;
