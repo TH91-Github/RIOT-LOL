6dd0e134895d5e57d22227a49276f48e83bd4482
김태훈 <je64768035@gmail.com>
2024-02-08 10:29:20 +0900

    fix: ios scrollbar 테스트 - 원복

--- src/component/RiotApp.jsx
+++ src/component/RiotApp.jsx
@@ -2,7 +2,6 @@ import { useCallback, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet, useLocation } from "react-router-dom";
 import { sSetMobileChk } from "store/store";
-import styled from "styled-components";
 // import { loadAxios } from "utils/fetchAxios";
 import { isMobile } from "utils/common.js"
 const RiotMain = () => {
@@ -26,61 +25,9 @@ const RiotMain = () => {
   return (
     <div className="wrap">
       <div className="container">
-        <TestWrap>
-          <div className="test">
-            <span>testsetset</span>
-            <span>testsetset</span>
-            <span>testsetset</span>
-            <span>testsetset</span>
-            <span>testsetset</span>
-            <span>testsetset</span>
-            <span>testsetset</span>
-            <span>testsetset</span>
-            <span>testsetset</span>
-          </div>
-        </TestWrap>
         <Outlet context={location} />
       </div>
     </div>
   )
 }
 export default RiotMain;
-
-const TestWrap = styled.div`
-  overflow-y:hidden;
-  overflow-x:auto;
-  position:absolute;
-  top:50px;
-  left:50%;
-  transform: translateX(-50%);
-  width:300px;
-  height:30px;
-  background:#999;
-  border:1px solid red;
-
-  &::-webkit-scrollbar-track {
-    border-radius: 3px;
-    background-color: black;
-  }
-  &::-webkit-scrollbar {
-    width: 10px;
-    height:5px;
-    border-radius: 3px;
-  }
-  &::-webkit-scrollbar-thumb {
-    border-radius: 3px;
-    background-color: red;
-  }
-
-  .test {
-    display:flex;
-    gap:10px;
-    align-items:center;
-    height:100%;
-    &> span{
-      padding:0 10px;
-      border:1px solid green;
-      border-radius:10px;
-    }
-  }
-`;
