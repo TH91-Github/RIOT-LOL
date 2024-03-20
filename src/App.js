66e9d1bc9cb42e3eb4555f4b6531d2e616b45821
김태훈 <je64768035@gmail.com>
2024-03-20 14:55:21 +0900

    Revert "test1"
    This reverts commit b9e2117bd6a4981134f0de533b24dc462b447a87.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,126 @@
 import RiotApp from 'component/RiotApp';
 import './App.css';
 import { GlobalStyles } from './assets/styles/GlobalStyles';
+import styled from 'styled-components';
+import { useEffect, useRef, useState } from 'react';
+
+
+
+let prevVisualViewport = 0
 
 function App() {
+  const [focusIn, setFocus] = useState(false);
+  const [heightV, setHeightV] =useState(0);
+
+  const isFocus = () => {
+    // setFocus(true);
+  }
+  const outFocus = () => {
+    // setFocus(false);
+  }
+
+  let fullHeight = useRef(0); // var fullHeight = 0; 
+  useEffect(() => { // 신경 x 로드 시 실행인 문법
+   
+    const handleVisualViewportResize = () => {
+      const scrollHeight = window.document.scrollingElement.scrollHeight
+
+      const scrollTop = scrollHeight - window.visualViewport.height
+
+      // window.scrollTo(0, scrollTop) // 입력창이 키보드에 가려지지 않도록 조절
+
+      prevVisualViewport = window.visualViewport.height  // 👈 이 부분이 키패드 나왔을때 높이
+      // 키패드 나왔을때 
+      // 1: btn wrap-> height: calc(prevVisualViewport - 버튼 높이)px; 
+      // 2: btn에 직접 position:fixed; top:calc(prevVisualViewport - 버튼 높이)px; 하면되지 않을까 생각중.
+
+      setHeightV(prevVisualViewport)
+    }
+    fullHeight.current = window.innerHeight;
+    visualViewport.addEventListener('resize', handleVisualViewportResize);
+      
+
+
+
+      return () => visualViewport.removeEventListener('resize', handleVisualViewportResize);
+
+
+
+  }, [])
+
   return (
     <div className="App">
       <GlobalStyles />
       {/* <RiotApp /> */}
+
+      <Wrap className={focusIn ? 'focus': ''}>
+        <BoxWrap1>
+          <input 
+            type="text" 
+            onFocus={()=>isFocus()}
+            onBlur={()=>outFocus()}
+            className="input" />
+            {heightV}
+        </BoxWrap1>
+        <BoxWrap1 $borderColor="yellow">
+          2번 박스
+        </BoxWrap1>
+        <BoxWrap1 $borderColor="pink">
+          3번 박스
+        </BoxWrap1>
+        <BoxHeight className="btn-box" $height={heightV}>
+          <button className="btn"> 충전이 곧 완료됩니다.</button>
+        </BoxHeight>
+      </Wrap>
     </div>
   );
 }
 
 export default App;
+
+const Wrap = styled.div`
+  display:flex;
+  flex-direction:column;
+  position:relative;
+  width:300px;
+  height:100vh;
+  margin:0 auto;
+  border:1px solid red;
+  text-align:center;
+  .input {
+    height:30px;
+    margin:50px auto 0;
+  }
+  .btn {
+    width:100%;
+    height:50px;
+    background:#000;
+    color:#fff;
+  }
+  &.focus {
+    .btn-box {
+      .btn{
+       
+      }
+    }
+  }
+`;
+
+const BoxWrap1 = styled.div`
+  height:200px;
+  border:1px solid ${props => props.$borderColor || 'green'};
+`;
+
+const BoxHeight = styled.div`
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  pointer-events : none;
+  height:${props => `${props.$height}px` || `height:100svh`};
+  .btn{
+    position:absolute;
+    left:0;
+    bottom:0;
+  }
+`;
